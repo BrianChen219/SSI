6c0e5f498a2cf40689b1a4245cd6a61e42ee3015
BrianChen <baotranquoc219@gmail.com>
2024-03-20 11:58:23 +0700

    del config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-auth_type = 'Bearer'
-consumerID = 'e10611b7369641c5a1e6784db34bab9e'
-consumerSecret = '07271fda44d448808dca7d419f3f1903'
-
-url = 'https://fc-data.ssi.com.vn/'
-stream_url = 'https://fc-datahub.ssi.com.vn/'
